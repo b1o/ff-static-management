cc6b6cd573f21314da2f90ca6fde3cf4d5bfe3c2
nyctoss <danail.stoyanov@gmail.com>
2026-01-04 13:07:34 +0200

    workaround cookie schema issue with derived and fix typos

--- packages/server/src/core/auth/auth.middleware.ts
+++ packages/server/src/core/auth/auth.middleware.ts
@@ -1,55 +1,46 @@
 import { Elysia } from "elysia";
 import { UnauthorizedError } from "../../lib/errors";
 import { SessionService } from "../session/session.service";
-import { authSessionCookie } from "./auth.model";
 
-export const requireAuth = new Elysia({ name: "requireAuth" })
-	.guard({
-		cookie: authSessionCookie,
-	})
-	.derive({ as: "scoped" }, async ({ cookie }) => {
-		const sessionId = cookie.auth_session?.value;
-		if (!sessionId) {
-			throw new UnauthorizedError();
-		}
+export const requireAuth = new Elysia({ name: "requireAuth" }).derive({ as: "scoped" }, async ({ cookie }) => {
+	const sessionId = cookie.auth_session?.value;
+	if (!sessionId) {
+		throw new UnauthorizedError();
+	}
 
-		const { session, user } = await SessionService.validate(sessionId as string);
-		if (!session || !user) {
-			throw new UnauthorizedError();
-		}
+	const { session, user } = await SessionService.validate(sessionId as string);
+	if (!session || !user) {
+		throw new UnauthorizedError();
+	}
 
-		if (session.fresh) {
-			const sessionCookie = SessionService.createCookie(session.id);
-			cookie.auth_session?.set({
-				value: sessionCookie.value,
-				...sessionCookie.attributes,
-			});
-		}
+	if (session.fresh) {
+		const sessionCookie = SessionService.createCookie(session.id);
+		cookie.auth_session?.set({
+			value: sessionCookie.value,
+			...sessionCookie.attributes,
+		});
+	}
 
-		return { session, user };
-	});
+	return { session, user };
+});
 
-export const optionalAuth = new Elysia({ name: "optionalAuth" })
-	.guard({
-		cookie: authSessionCookie,
-	})
-	.derive({ as: "scoped" }, async ({ cookie }) => {
-		const sessionId = cookie.auth_session?.value;
-		if (!sessionId) {
-			return { session: null, user: null };
-		}
+export const optionalAuth = new Elysia({ name: "optionalAuth" }).derive({ as: "scoped" }, async ({ cookie }) => {
+	const sessionId = cookie.auth_session?.value;
+	if (!sessionId) {
+		return { session: null, user: null };
+	}
 
-		const { session, user } = await SessionService.validate(sessionId as string);
-		if (!session || !user) {
-			return { session: null, user: null };
-		}
-		if (session.fresh) {
-			const sessionCookie = SessionService.createCookie(session.id);
-			cookie.auth_session?.set({
-				value: sessionCookie.value,
-				...sessionCookie.attributes,
-			});
-		}
+	const { session, user } = await SessionService.validate(sessionId as string);
+	if (!session || !user) {
+		return { session: null, user: null };
+	}
+	if (session.fresh) {
+		const sessionCookie = SessionService.createCookie(session.id);
+		cookie.auth_session?.set({
+			value: sessionCookie.value,
+			...sessionCookie.attributes,
+		});
+	}
 
-		return { session, user };
-	});
+	return { session, user };
+});
